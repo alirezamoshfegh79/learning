4bee59542c1f7b1c56370ead2d9dd881a3192d69
Alireza Mousavimoshfegh <alirezamosavi277@gmail.com>
2025-04-03 15:55:06 -0300

    __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ def create_app():
     app = Flask(__name__)
     api = Api(app)
     
-    # Import routes after app creation to avoid circular imports
+   
     from .routes import HelloWorld
     
     api.add_resource(HelloWorld, '/')
